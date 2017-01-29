db0e4e75b8b48a8cd3121fdebccdcc4ca10bf2f1
Dejian <wdj10211599@gmail.com>
2017-01-29 17:51:14 -0500

    my_own_folder

--- my_own_folder/test.R
+++ my_own_folder/test.R
@@ -1,3 +1,12 @@
+library(DT)
+datatable(iris, options = list(pageLength = 5))
+
 require(devtools)
 install_github('rCharts', 'ramnathv')
-
+library(rCharts)
+## Example 1 Facetted Scatterplot
+names(iris) = gsub("\\.", "", names(iris))
+rPlot(SepalLength ~ SepalWidth | Species, data = iris, color = 'Species', type = 'point')
+## Example 2 Facetted Barplot
+hair_eye = as.data.frame(HairEyeColor)
+rPlot(Freq ~ Hair | Eye, color = 'Eye', data = hair_eye, type = 'bar')
